5c0bd0f4b6e57b2e0a53c6df8de4b43c9fa27f92
cecep31 <cecepjanuardi31@gmail.com>
2021-05-17 22:12:00 +0700

    first nextjs

---
+++ components/head.js
@@ -0,0 +1,9 @@
+const head = () => {
+    return (
+        <div>
+             <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        </div>
+    )
+}
+
+export default head
---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Button} from '@material-ui/core';
+import Head from '../components/head';
+
+const about = () => {
+    return (
+        <div>
+            <Head/>
+            <Button variant="contained" size="small" color="primary">sssss</Button>
+        </div>
+    )
+}
+
+
+
+export default about
