80f2267a88b8720d1f0d262a453c58c497f25525
cecep31 <cecepjanuardi31@gmail.com>
2021-05-18 11:25:08 +0700

    add tailwind

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,20 @@
-import React from 'react';
 import {Button} from '@material-ui/core';
-import Head from '../components/head';
+import Head from 'next/head';
 
 const about = () => {
     return (
         <div>
-            <Head/>
-            <Button variant="contained" size="small" color="primary">sssss</Button>
+            <Head>
+                <title>About</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+            </Head>
+            <main>
+                <div className=""></div>
+                <Button variant="contained" size="small" color="primary">sssss</Button>
+            </main>
         </div>
     )
 }
-
-
 
 export default about
